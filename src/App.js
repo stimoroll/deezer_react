cf63f4e8539f66eba30c67127ff310a913bdab52
Poplawski, Krzysztof <kpoplawski@rxsense.com>
2021-03-24 08:09:59 +0100

    add autocomplete list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import React, { useEffect, useState, useRef } from 'react';
 import axios from 'axios';
+import _ from 'lodash';
 import './App.css';
 
 const search_endpint = (search) => `https://proxy.kpoplawski.repl.co/search/${search}`;
 
 function App() {
   const input_ref = useRef()
-  const [search_query, setSearchQuery] = useState('enya');
+  const [search_query, setSearchQuery] = useState('');
   const [input_value, setInputValue] = useState('');
+
+  const debouncedSendQuery = _.throttle(setSearchQuery, 2000);
+
+  // const [result_list, fillResultList] = useState(null);
+  const [result_list, fillResultList] = useState([]);
+
 
   const handleInputChange = (e) => {
     console.log(e);
     setInputValue(e.target.value);
-    setSearchQuery(e.target.value);
+
+    debouncedSendQuery(e.target.value);
   }
   useEffect(() => {
-    // if (search_query && search_query.length > 0) {
+    if (search_query && search_query.length > 0) {
       axios.get(search_endpint(search_query))
-        .then(response => console.log(response))
+        .then(response => {
+          fillResultList(response.data.data);
+          console.log(response.data.data);
+        })
         .catch(error => console.log(error));
-    // }
+    }
   }, [search_query])
   return (
     <div className="App">
@@ -31,6 +42,9 @@ function App() {
           ref={input_ref}
           onChange={handleInputChange}
         />
+        <ul>
+          {result_list.map(record => <li key={record.key}>{record.name}</li>)}
+        </ul>
 
       </header>
     </div>
