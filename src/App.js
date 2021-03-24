983ae69c465b8dfa56890efb630343a984ceb814
Poplawski, Krzysztof <kpoplawski@rxsense.com>
2021-03-24 08:16:52 +0100

    set serch and artist throttled services

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import _ from 'lodash';
 import './App.css';
 
 const search_endpint = (search) => `https://proxy.kpoplawski.repl.co/search/${search}`;
+const artist_endpint = (artist) => `https://proxy.kpoplawski.repl.co/artist/${artist}`;
 
 function App() {
   const input_ref = useRef()
@@ -15,6 +16,25 @@ function App() {
   // const [result_list, fillResultList] = useState(null);
   const [result_list, fillResultList] = useState([]);
 
+  const searchArtistService = _.throttle((search_query, callback) => {
+    axios.get(search_endpint(search_query))
+      .then(response => {
+        // fillResultList(response.data.data);
+        callback(response.data.data);
+        console.log(response.data.data);
+      })
+      .catch(error => console.log(error));
+  }, 1000);
+
+  const getArtistService = _.throttle((artist_id, callback) => {
+      axios.get(artist_endpint(artist_id))
+        .then(response => {
+          // fillResultList(response.data.data);
+          callback(response.data.data);
+          console.log(response.data.data);
+        })
+        .catch(error => console.log(error));
+  }, 300);
 
   const handleInputChange = (e) => {
     console.log(e);
@@ -24,14 +44,19 @@ function App() {
   }
   useEffect(() => {
     if (search_query && search_query.length > 0) {
-      axios.get(search_endpint(search_query))
-        .then(response => {
-          fillResultList(response.data.data);
-          console.log(response.data.data);
-        })
-        .catch(error => console.log(error));
+      searchArtistService(search_query, fillResultList);
+      // axios.get(search_endpint(search_query))
+      //   .then(response => {
+      //     fillResultList(response.data.data);
+      //     console.log(response.data.data);
+      //   })
+      //   .catch(error => console.log(error));
     }
-  }, [search_query])
+  }, [search_query]);
+
+  useEffect(() => {
+    input_ref.current.focus();
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
