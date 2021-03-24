570fbce226dc1868e338a9d5bc12468fc59d48b3
Poplawski, Krzysztof <kpoplawski@rxsense.com>
2021-03-24 07:32:56 +0100

    set proxy connection

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState, useRef } from 'react';
+import axios from 'axios';
 import './App.css';
 
+const search_endpint = (search) => `https://proxy.kpoplawski.repl.co/search/${search}`;
+
 function App() {
+  const input_ref = useRef()
+  const [search_query, setSearchQuery] = useState('enya');
+  const [input_value, setInputValue] = useState('');
+
+  const handleInputChange = (e) => {
+    console.log(e);
+    setInputValue(e.target.value);
+    setSearchQuery(e.target.value);
+  }
+  useEffect(() => {
+    if (search_query && search_query.length > 0) {
+      axios.get(search_endpint(search_query)).then(response => console.log(response));
+    }
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code>!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input
+          type="text"
+          className="input__search"
+          value={input_value}
+          ref={input_ref}
+          onChange={handleInputChange}
+        />
+
       </header>
     </div>
   );
